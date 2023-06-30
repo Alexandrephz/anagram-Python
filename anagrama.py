f5fba8cddd68c42028ccc74e55b1daad7f4185e5
alexandrephz <alexandre_gomes@live.com>
2023-06-30 10:58:21 -0300

    Performance improved

--- anagrama.py
+++ anagrama.py
@@ -1,61 +1,47 @@
 import random
+from collections import Counter
 
-class Word:
+class ShuffleWord:
     def __init__(self, word):
         self.word = word
-
-    @property
-    def word(self):
-        return self.__word
-    
-    @word.setter
-    def word(self, new_word):
-        if isinstance(new_word, str):
-            if len(new_word) > 1:
-                self.__word = new_word
-            else:
-                raise ValueError("Value cannot be null or have 1 digit !")
-        else:
-            raise TypeError("Wrong type !")
-
-    def word_size(self):
-        word_size = len(self.__word)
-        return word_size
-
-class ShuffleWord(Word):
-    def __init__(self, word):
-        super().__init__(word)
-
-    def fatorial_number(self):
+        self.word_size = len(word)
         self.fatorial = 1
-        for x in range(super().word_size(),1,-1):
-            self.fatorial *= x
-        return self.fatorial
-    
-    def duplicated_exclusion(self):
-        duplicated_word = {}
-        self.fatorial_exclude = 1
-        for x in self.word:
-            if self.word.count(x) > 1:
-                duplicated_word[x] = self.word.count(x)
-        for value in duplicated_word:
-            self.fatorial_exclude *= duplicated_word[value]
-        return self.fatorial_exclude
-        
+        self.duplicated_exclusion = 1
+
+    def calculate_factorial(self, n):
+        factorial = 1
+        for i in range(2, n + 1):
+            factorial *= i
+        return factorial
+
+    def calculate_duplicated_exclusion(self):
+        letter_counts = Counter(self.word)
+        for count in letter_counts.values():
+            if count > 1:
+                self.duplicated_exclusion *= self.calculate_factorial(count)
+        return self.duplicated_exclusion
+
+    def generate_anagrams(self):
+        anagram_list = []
+        self.generate_anagrams_helper(list(self.word), len(self.word), anagram_list)
+        return anagram_list
+
+    def generate_anagrams_helper(self, word_list, n, anagram_list):
+        if n == 1:
+            anagram = ''.join(word_list)
+            anagram_list.append(anagram)
+        else:
+            for i in range(n):
+                word_list[i], word_list[n - 1] = word_list[n - 1], word_list[i]
+                self.generate_anagrams_helper(word_list, n - 1, anagram_list)
+                word_list[i], word_list[n - 1] = word_list[n - 1], word_list[i]
 
     def possibility_calculator(self):
-        anagram_list = []
-        anagram_list.append(self.word)
-        anagram_size = (self.fatorial_number() / self.duplicated_exclusion()) - 1
-        while anagram_size != 0:
-            shuffle = ''.join(random.sample(self.word, len(self.word)))
-            if not shuffle in anagram_list:
-                anagram_list.append(shuffle)
-                anagram_size -= 1
-            else:
-                continue
+        total_anagrams = self.calculate_factorial(self.word_size) // self.calculate_duplicated_exclusion()
+        anagram_list = self.generate_anagrams()
+        random.shuffle(anagram_list)
+        return anagram_list[:total_anagrams]
 
-        return anagram_list
 
 if __name__ == "__main__":
     print("Input:")
